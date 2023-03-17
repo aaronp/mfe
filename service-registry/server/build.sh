00d5521b7c7731f981c408b284d37722817f8632
Aaron Pritzlaff <aaron.pritzlaff.freelancer@theguardian.com>
2023-03-17 10:16:09 +0000

    server-registry server building a docker image

--- service-registry/server/build.sh
+++ service-registry/server/build.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 export IMG=${IMG:-service-registry}
-scala-cli --power package --docker App.scala --docker-from openjdk:11 --docker-image-repository service-registry
+
+
+# scala-cli --power package --docker App.scala --docker-from openjdk:11 --docker-image-repository service-registry
+docker build --tag $IMG .
 
 echo "built $IMG"
+echo
 echo "run with:"
-echo "PORT=1234 && docker run -it --rm -name service-registry -p $PORT:$PORT $IMG:latest"
+echo ""
+echo "PORT=1234 && docker run -it --rm -p 1234:1234 -d $IMG:latest"
+echo ""
+echo "test with:"
+echo ""
+echo "curl -X POST -d '{\"webComponent\":{\"url\":\"dave\",\"component\":\"susan\"},\"label\":\"example\",\"tags\":[\"some\",\"app\"]}' http://localhost:1234/api/v1/registry/bar"
