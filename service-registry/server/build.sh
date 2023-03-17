eec202b3118559179fb43ef2fef5360a80769573
Aaron Pritzlaff <aaron.pritzlaff.freelancer@theguardian.com>
2023-03-17 10:30:06 +0000

    added makefile

--- service-registry/server/build.sh
+++ service-registry/server/build.sh
@@ -1,16 +1,36 @@
 #!/usr/bin/env bash
 export IMG=${IMG:-service-registry}
+export PORT=${PORT:-8080}
 
 
 # scala-cli --power package --docker App.scala --docker-from openjdk:11 --docker-image-repository service-registry
-docker build --tag $IMG .
 
-echo "built $IMG"
-echo
-echo "run with:"
-echo ""
-echo "PORT=1234 && docker run -it --rm -p 1234:1234 -d $IMG:latest"
-echo ""
-echo "test with:"
-echo ""
-echo "curl -X POST -d '{\"webComponent\":{\"url\":\"dave\",\"component\":\"susan\"},\"label\":\"example\",\"tags\":[\"some\",\"app\"]}' http://localhost:1234/api/v1/registry/bar"
+
+build() {
+    echo "building $IMG ..."
+    docker build --tag $IMG .
+    echo "built $IMG"
+}
+
+run() {
+    id=`docker run -it --rm -p $PORT:$PORT -d $IMG:latest`
+    echo "Running on port $PORT"
+    echo "stop using ./kill.sh"
+    cat > kill.sh <<EOL
+docker kill $id
+# clean up after ourselves
+rm kill.sh
+EOL
+    chmod +x kill.sh
+}
+
+test() {
+    echo
+    echo "run with:"
+    echo ""
+    echo "docker run -it --rm -p $PORT:$PORT -d $IMG:latest"
+    echo ""
+    echo "test with:"
+    echo ""
+    echo "curl -X POST -d '{\"webComponent\":{\"url\":\"dave\",\"component\":\"susan\"},\"label\":\"example\",\"tags\":[\"some\",\"app\"]}' http://localhost:$PORT/api/v1/registry/bar"
+}
