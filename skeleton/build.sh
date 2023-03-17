f6311ce98f75cb4e473eedb2343bed0f90efecf7
Aaron Pritzlaff <aaron.pritzlaff.freelancer@theguardian.com>
2023-03-17 22:12:40 +0000

    getting sveltekit in Docker

---
+++ skeleton/build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+docker build . -t skeleton-debug
+
+docker run -dp 3000:80 svelte-docker-static
--- skeleton/svelte-docker/svelte.config.js
+++ skeleton/svelte-docker/svelte.config.js
@@ -14,9 +14,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapterStatic(),
 
-		methodOverride : {
-			allowed: ['PATCH', 'DELETE']
-		}
+		// methodOverride : { allowed: ['PATCH', 'DELETE'] }
 	}
 };
 
