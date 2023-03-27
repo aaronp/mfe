359f5002b13869b32a9361897fa91e4a83754d6d
Aaron Pritzlaff <aaron.pritzlaff.freelancer@theguardian.com>
2023-03-27 15:29:54 +0100

    bff changes

--- dashboard/web/src/routes/api/component/[id]/+server.ts
+++ dashboard/web/src/routes/api/component/[id]/+server.ts
@@ -3,7 +3,10 @@ import { json, type RequestHandler } from '@sveltejs/kit'
 export const GET: RequestHandler = async (event) => {
   const segments = event.url.pathname.split('/');
   const id = segments[segments.length -1 ];
-  const url = new URL(`component/${id}`, "http://localhost:8081");
+
+  // TODO - config via env
+  // const url = new URL(`component/${id}`, "http://localhost:8081");
+  const url = new URL(`component/${id}`, "http://dashboard-bff-service.mfe:8080");
   const r = await fetch(url);
   return r;
 }
--- dashboard/web/src/routes/api/component/[id]/bundle.[ext]/+server.ts
+++ dashboard/web/src/routes/api/component/[id]/bundle.[ext]/+server.ts
@@ -5,7 +5,8 @@ export const GET: RequestHandler = async (event) => {
   const id = segments[segments.length - 2];
   const file = segments[segments.length - 1];
   console.log(`proxy getting ${file} from ${id} `);
-  const r = await fetch(new URL(`component/${id}/${file}`, "http://localhost:8081"));
+  // TODO - config via env
+  const r = await fetch(new URL(`component/${id}/${file}`, "http://dashboard-bff-service.mfe:8080");
   return r;
 }
 
